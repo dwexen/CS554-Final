b804ed60a9b0f1a763c94887e0acdc0f7c0481fd
patgrasso <pgrasso@stevens.edu>
2017-05-13 15:12:23 -0400

    Change db name to something more specific & relevant

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const pages = data.pages;
 
 var testWords = ["stemming", "technology", "new", "computer", "programming", "science", "politics", "trump"];
 //crawler.startCrawl("http://www.arstechnica.com", testWords, 10);
-//crawler.startCrawl("http://www.arstechnica.com", testWords, 40, .60);
+crawler.startCrawl("http://www.arstechnica.com", testWords, 40, .60);
