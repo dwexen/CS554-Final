4669b44e9a379aa4c27d868eba735695886ed161
Julia Kim <Julia Kim>
2017-05-15 14:08:58 -0400

    generated list usint dummy data, broken sort mechanism

--- public/js/components.js
+++ public/js/components.js
@@ -1,16 +1,53 @@
 "use strict";
 
 var AppComponent = function AppComponent() {
+
     return React.createElement(
-        "div",
-        { className: "row" },
+        "div", {
+            className: "row"
+        },
         React.createElement(
-            "div",
-            { className: "col-sm-8" },
-            React.createElement(PagesContainer, { url: "/pages" })
+            "div", {
+                className: "col-sm-12"
+            },
+            React.createElement(PagesContainer, {
+                url: "/pages"
+            })
         )
     );
 };
+
+"use strict";
+
+var SettingsContainer = React.createClass({
+    displayName: "SettingsContainer",
+    sortPages: function sortPages(field) {
+        this.props.sortPagesBy(this.props.pagesState, field, this.props.pages, this.props.direction);
+    },
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "sort-section" },
+            React.createElement(
+                "h1",
+                null,
+                "Sort",
+                React.createElement("br", null),
+                "by"
+            ),
+            React.createElement(
+                "div",
+                { className: "pill", onClick: this.sortPages.bind(this, 'relevance') },
+                "Relevance"
+            ),
+            React.createElement(
+                "div",
+                { className: "pill", onClick: this.sortPages.bind(this, 'title') },
+                "Title"
+            )
+        );
+    }
+});
 
 "use strict";
 
@@ -18,7 +55,9 @@ var PagesContainer = React.createClass({
     displayName: "PagesContainer",
 
     getInitialState: function getInitialState() {
-        return { pages: [] };
+        return {
+            pages: []
+        };
     },
 
     componentDidMount: function componentDidMount() {
@@ -29,7 +68,9 @@ var PagesContainer = React.createClass({
             dataType: 'json',
             cache: false,
             success: function success(pagesList) {
-                _this.setState({ pages: pagesList });
+                _this.setState({
+                    pages: pagesList
+                });
             },
             error: function error(xhr, status, err) {
                 console.error(_this.props.url, status, err.toString());
@@ -37,71 +78,111 @@ var PagesContainer = React.createClass({
         });
     },
 
+    sortPagesBy: function (listState, field, pages, direction) {
+        // Sorting ...
+        pages.sort(function (a, b) {
+            if (a[field] > b[field])
+                return -direction;
+            if (a[field] < b[field])
+                return direction;
+            return 0;
+        });
+        // Change state
+        listState.setState({ 'pages': pages, 'direction': -direction });
+    },
+
     render: function render() {
         return React.createElement(
-            "div",
-            { className: "page" },
-            React.createElement(PagesList, { pages: this.state.pages })
-        );
+            "row",
+            null,
+            React.createElement(
+                "div",
+                { className: "col-sm-3" },
+                React.createElement(SettingsContainer, {
+                    pagesState: this, direction: this.state.direction, pages: this.state.pages, sortPagesBy: this.sortPagesBy
+                })
+            ),
+            React.createElement(
+                "div", {
+                    className: "col-sm-9 page"
+                },
+                React.createElement(PagesList, {
+                    pages: this.state.pages
+                })
+            )
+        )
     }
 });
 "use strict";
 
 var Page = function Page(_ref) {
-  var title = _ref.title,
-      url = _ref.url,
-      relevancy = _ref.relevancy,
-      description = _ref.description;
-    //   topics = _ref.topics
+    var title = _ref.title,
+        url = _ref.url,
+        relevancy = _ref.relevancy,
+        description = _ref.description,
+        topics = _ref.topics;
 
-//   var viewTopics = topics.map(function (topic) {
-//     return React.createElement(
-//       "li",
-//       null,
-//       topic
-//     );
-//   });
+    var viewTopics = topics.map(function(topic) {
+        return React.createElement(
+            "li", {
+                className: "tag text-muted"
+            },
+            topic
+        );
+    });
 
-  return React.createElement(
-    "div",
-    { className: "panel panel-default" },
-    React.createElement(
-      "div",
-      { className: "panel-heading" },
-      url
-    ),
-    React.createElement(
-      "div",
-      { className: "panel-body" },
-      React.createElement(
-        "div",
-        null,
+    return React.createElement(
+        "div", {
+            className: "panel panel-default"
+        },
         React.createElement(
-          "h1",
-          null,
-          title
+            "div", {
+                className: "panel-heading"
+            },
+            url
         ),
         React.createElement(
-          "p",
-          null,
-          description
+            "div", {
+                className: "panel-body"
+            },
+            React.createElement(
+                "div",
+                null,
+                React.createElement(
+                    "h1",
+                    null,
+                    title
+                ),
+                React.createElement(
+                    "p",
+                    null,
+                    description
+                ),
+                React.createElement(
+                    "div", {
+                        className: "row"
+                    },
+                    React.createElement(
+                        "div", {
+                            className: "col-sm-12"
+                        },
+                        React.createElement(
+                            "ul", {
+                                className: "tags"
+                            },
+                            React.createElement(
+                                "i", {
+                                    className: "fa fa-tag"
+                                },
+                                null
+                            ),
+                            viewTopics
+                        )
+                    )
+                )
+            )
         )
-        // React.createElement(
-        //   "div",
-        //   { className: "row" },
-        //   React.createElement(
-        //     "div",
-        //     { className: "col-md-8" },
-        //     React.createElement(
-        //       "ol",
-        //       null,
-        //       viewTopics
-        //     )
-        //   )
-        // )
-      )
-    )
-  );
+    );
 };
 'use strict';
 
@@ -111,16 +192,16 @@ var PagesList = function PagesList(_ref) {
     return React.createElement(
         "div",
         null,
-        pages.map(function (page) {
+        pages.map(function(page) {
             return React.createElement(Page, {
                 key: page.id,
                 id: page.id,
                 title: page.title,
                 url: page.url,
                 description: page.description,
-                relevancy: page.relevancy
-             });
-                // topics: page.topics
+                relevancy: page.relevancy,
+                topics: page.topics
+            });
         })
     );
 };
--- routes/pages.js
+++ routes/pages.js
@@ -2,23 +2,19 @@ const express = require('express');
 const router = express.Router();
 const data = require("../data");
 const pages = data.pages;
+const sampleData = require("../sampleData.json");
 
 router.get("/pages", (req, res) => {
-    pages.getAllPages().then((pagesList) => {
-        res.json(pagesList);
-    }).catch(() => {
-        // Something went wrong with the server!
-        res.sendStatus(500);
-    });
-});
-
-router.get("/", (req, res) => {
+    res.json(sampleData);
     // pages.getAllPages().then((pagesList) => {
-    //     res.render("home", {});
+    //     res.json(pagesList);
     // }).catch(() => {
     //     // Something went wrong with the server!
     //     res.sendStatus(500);
     // });
+});
+
+router.get("/", (req, res) => {
     res.render("home", {});
 });
 
